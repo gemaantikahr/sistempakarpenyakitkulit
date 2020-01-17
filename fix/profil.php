91910c5e5f189b5359445afd42ae47a17a8871e7
Gema Antika Hariadi <gemaantikahr@gmail.com>
2020-01-17 21:14:57 +0700

    semangat ya

---
+++ fix/profil.php
@@ -0,0 +1,86 @@
+<?php include "conn.php"; ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="SISTEM PAKAR DIAGNOSA PENYAKIT SAYUR ORGANIK | www.hakkoblogs.com">
+    <link rel="icon" href="">
+
+    <title>SISTEM PAKAR DIAGNOSA PENYAKIT SAYUR ORGANIK | www.hakkoblogs.com</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/justified-nav.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <?php include "header.php"; ?>
+
+      <main role="main">
+
+        <!-- Example row of columns -->
+        <div class="row">
+          <div class="col-lg-12">
+            <h2>Profile</h2>
+            <table class="table table-hover table-condensed">
+            <tr>
+            <td>Aplikasi</td>
+            <td>: Sistem Pakar Diagnosa Penyakit Tanaman Sayur</td>
+
+            </tr>
+            <tr>
+            <td>Pembuat</td>
+            <td>: 1. Gema antika hariadi // 1600018095<br>: 2. Irfan afandi // 1600018085<br>: 3. Ardiansyah // 1600018058</td>
+            </tr>
+            <tr>
+            </table>
+          </div>
+        </div>
+<br /><br />
+      </main>
+
+      <!-- Modal Popup -->
+      <div id="myModal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                
+                </div>
+                <div class="modal-body">
+                    <p>Selamat datang di sistem pakar gejala penyakit pada tumbuhan.</p>
+                    <p>Di aplikasi ini anda bisa mengetahui penyakit tumbuhan dengan gejala yang ditimbulkannya</p>
+                    <b>Sistem Pakar Penyakit Tumbuhan</b>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- end Modal Popup -->
+
+      <!-- Site footer -->
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- <script src="js/jquery-slim.min.js"></script> -->
+    <!-- <script>window.jQuery || document.write('<script src="js/jquery.js"><\/script>')</script> -->
+    <script src="js/popper.min.js"></script>
+    <script src="js/jquery-slim.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  <!--  <script type="text/javascript">
+      $(document).ready(function(){
+          $("#myModal").modal('show');
+      });
+  </script>-->
+  </body>
+</html>
